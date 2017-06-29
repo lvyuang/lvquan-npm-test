b519982fea86a0dc46bb70bef0b120ce59c1c2a2
lvyuang <yuang2008@gmail.com>
2017-06-29 17:28:31 +0800

    增加 umd 支持，处理 es6 default

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,25 @@
-'use strict';
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(['module', 'exports'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(module, exports);
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod, mod.exports);
+    global.index = mod.exports;
+  }
+})(this, function (module, exports) {
+  'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  exports.default = function () {
+    return 'Hello, this is my second version.';
+  };
+
+  module.exports = exports['default'];
 });
-
-exports.default = function () {
-  return 'Hello, this is my second version.';
-};
